e5a02e20958f19d65dc65bddc16d1a572339780c
pankajdahilkar <pankajmdahilkar@gmail.com>
2019-03-25 20:20:19 +0530

    mute

---
+++ mute_deaf.py
@@ -0,0 +1,15 @@
+import os,time
+import serial
+
+def robot(text):
+	os.system("espeak -ven-us+f4-s160 ' "+text+" ' 2>/dev/null")
+
+ser=serial.Serial("/dev/ttyS0",9600)
+time.sleep(3)
+robot("System initialise")
+time.sleep(2)
+while True:
+	if(ser.in_waiting>0):
+		line = ser.readline()
+		print(line)
+		robot(line)
